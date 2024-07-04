1dbc19cf63f23490f5c5c3bf6e758dc575ed988d
David Yoon <davidyoon@hanwha.com>
2024-07-04 12:55:59 +0900

    Initial Commit

---
+++ LazyIssue/LazyIssueApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyIssueApp.swift
+//  LazyIssue
+//
+//  Created by Davidyoon on 7/4/24.
+//
+
+import SwiftUI
+
+@main
+struct LazyIssueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
